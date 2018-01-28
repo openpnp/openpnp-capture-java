e8b5cfb6654506786a83314162e571da286ba157
Jason von Nieda <jason@vonnieda.org>
2018-01-28 11:18:55 -0600

    Release v0.0.12 with new properties.

--- src/main/java/org/openpnp/capture/CaptureProperty.java
+++ src/main/java/org/openpnp/capture/CaptureProperty.java
@@ -3,13 +3,17 @@ package org.openpnp.capture;
 import org.openpnp.capture.library.OpenpnpCaptureLibrary;
 
 public enum CaptureProperty {
+    BackLightCompensation(OpenpnpCaptureLibrary.CAPPROPID_BACKLIGHTCOMP),
     Brightness(OpenpnpCaptureLibrary.CAPPROPID_BRIGHTNESS),
     Contrast(OpenpnpCaptureLibrary.CAPPROPID_CONTRAST),
     Exposure(OpenpnpCaptureLibrary.CAPPROPID_EXPOSURE),
     Focus(OpenpnpCaptureLibrary.CAPPROPID_FOCUS),
     Gain(OpenpnpCaptureLibrary.CAPPROPID_GAIN),
     Gamma(OpenpnpCaptureLibrary.CAPPROPID_GAMMA),
+    Hue(OpenpnpCaptureLibrary.CAPPROPID_HUE),
+    PowerLineFrequency(OpenpnpCaptureLibrary.CAPPROPID_POWERLINEFREQ),
     Saturation(OpenpnpCaptureLibrary.CAPPROPID_SATURATION),
+    Sharpness(OpenpnpCaptureLibrary.CAPPROPID_SHARPNESS),
     WhiteBalance(OpenpnpCaptureLibrary.CAPPROPID_WHITEBALANCE),
     Zoom(OpenpnpCaptureLibrary.CAPPROPID_ZOOM)
     ;
