648012b8044efd509d9d8b26baf89152f2c58525
Jason von Nieda <jason@vonnieda.org>
2019-01-17 21:09:31 -0600

    Optimizes creation of the images that are returned from the stream by attempting to use the operating system's native image format. This helps by potentially skipping a color space conversion in Java which is pretty slow. This code all works and is tested for all the supported image formats, but I'm not happy with the copy/pasting of all the BufferedImage types. Need to rework that bit into a hash or something. This saves about 3% CPU on my Mac when running 1280x720@30 FPS with a refresh rate of 10 FPS.

--- src/main/java/org/openpnp/capture/CaptureStream.java
+++ src/main/java/org/openpnp/capture/CaptureStream.java
@@ -1,6 +1,11 @@
 package org.openpnp.capture;
 
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
 import java.nio.IntBuffer;
 
 import org.openpnp.capture.library.OpenpnpCaptureLibrary;
@@ -13,15 +18,43 @@ public class CaptureStream {
     final private int streamId;
     final private CaptureFormat format;
     final private Memory memory;
+    private int imageType;
 
     public CaptureStream(Pointer context, int streamId, CaptureFormat format) {
         this.context = context;
         this.streamId = streamId;
         this.format = format;
-        memory = new Memory(format.formatInfo.width * format.formatInfo.height * 3);
+        imageType = getBufferedImageType();
+        if (imageType == BufferedImage.TYPE_3BYTE_BGR) {
+            memory = new Memory(format.formatInfo.width * format.formatInfo.height * 3);
+        }
+        else {
+            memory = new Memory(format.formatInfo.width * format.formatInfo.height * 4);
+        }
         memory.clear();
     }
-
+    
+    private int getBufferedImageType() {
+        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice dev = env.getDefaultScreenDevice();
+        GraphicsConfiguration config = dev.getDefaultConfiguration();
+        BufferedImage image = config.createCompatibleImage(format.formatInfo.width, 
+                format.formatInfo.height);
+        int type = image.getType();
+        if (type == BufferedImage.TYPE_3BYTE_BGR 
+                || type == BufferedImage.TYPE_4BYTE_ABGR
+                || type == BufferedImage.TYPE_4BYTE_ABGR_PRE
+                || type == BufferedImage.TYPE_INT_ARGB
+                || type == BufferedImage.TYPE_INT_ARGB_PRE 
+                || type == BufferedImage.TYPE_INT_BGR
+                || type == BufferedImage.TYPE_INT_RGB) {
+            return type;
+        }
+        else {
+            return BufferedImage.TYPE_INT_RGB;
+        }
+    }
+    
     public void close() {
         OpenpnpCaptureLibrary.INSTANCE.Cap_closeStream(context, streamId);
     }
@@ -88,29 +121,82 @@ public class CaptureStream {
     }
 
     public BufferedImage capture() throws Exception {
-        byte[] bytes;
-
         synchronized (memory) {
             int result = OpenpnpCaptureLibrary.INSTANCE.Cap_captureFrame(context, streamId, memory,
                     (int) memory.size());
             if (result != OpenpnpCaptureLibrary.CAPRESULT_OK) {
                 throw new CaptureException(result);
             }
-            bytes = memory.getByteArray(0, (int) memory.size());
+            
+            BufferedImage image = new BufferedImage(format.formatInfo.width, format.formatInfo.height,
+                    imageType);
+            switch (imageType) {
+                case BufferedImage.TYPE_3BYTE_BGR: 
+                    return convertRgbTo3ByteBgr(memory, image);
+                case BufferedImage.TYPE_4BYTE_ABGR:
+                case BufferedImage.TYPE_4BYTE_ABGR_PRE:
+                    return convertRgbTo4ByteBgr(memory, image);
+                case BufferedImage.TYPE_INT_ARGB:
+                case BufferedImage.TYPE_INT_ARGB_PRE: 
+                case BufferedImage.TYPE_INT_RGB:
+                    return convertRgbToIntRgbBgr(memory, image, false);
+                case BufferedImage.TYPE_INT_BGR:
+                    return convertRgbToIntRgbBgr(memory, image, true);
+                default:
+                    throw new Error("Programmer error: Unknown image type.");
+            }
         }
-
-        // Note: I assumed that setting an RGB buffer on a BGR image would result in R and B
-        // swapped, but that doesn't seem to happen. I tested with manually converting
-        // to ARGB and that worked too, so now my working assumption is that setDataElements
-        // always expects RGB and converts to the BufferedImage's color model.
-        BufferedImage image = new BufferedImage(format.formatInfo.width, format.formatInfo.height,
-                BufferedImage.TYPE_3BYTE_BGR);
-        image.getRaster()
-             .setDataElements(0, 0, image.getWidth(), image.getHeight(), bytes);
-
+    }
+    
+    private BufferedImage convertRgbToIntRgbBgr(Memory memory, BufferedImage image, boolean bgr) {
+        byte[] rgb = memory.getByteArray(0, (int) memory.size());
+        // Get a reference to the image's raster array.
+        int[] imageData = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+        for (int i = 0; i < format.formatInfo.width * format.formatInfo.height; i++) {
+            int a = 0xff;
+            int r = rgb[i * 3 + 0] & 0xff;
+            int g = rgb[i * 3 + 1] & 0xff;
+            int b = rgb[i * 3 + 2] & 0xff;
+            if (bgr) {
+                imageData[i] = (a << 24) | (b << 16) | (g << 8) | (r << 0);
+            }
+            else {
+                imageData[i] = (a << 24) | (r << 16) | (g << 8) | (b << 0);
+            }
+        }
         return image;
     }
     
+    private BufferedImage convertRgbTo3ByteBgr(Memory memory, BufferedImage image) {
+        // Get a reference to the image's raster array.
+        byte[] imageData = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        // Copy from the captured frame memory to the raster.
+        memory.read(0, imageData, 0, (int) memory.size());
+        // Swap R and B since the capture data is RGB and we created a BGR image.
+        for (int i = 0; i < format.formatInfo.width * format.formatInfo.height * 3; i += 3) {
+            byte r = imageData[i];
+            imageData[i] = imageData[i + 2];
+            imageData[i + 2] = r;
+        }
+        return image;
+    }
+
+    private BufferedImage convertRgbTo4ByteBgr(Memory memory, BufferedImage image) {
+        byte[] rgb = memory.getByteArray(0, (int) memory.size());
+        // Get a reference to the image's raster array.
+        byte[] imageData = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        for (int i = 0; i < format.formatInfo.width * format.formatInfo.height; i++) {
+            int r = rgb[i * 3 + 0] & 0xff;
+            int g = rgb[i * 3 + 1] & 0xff;
+            int b = rgb[i * 3 + 2] & 0xff;
+            imageData[i * 4 + 0] = (byte) 0xff;
+            imageData[i * 4 + 1] = (byte) b;
+            imageData[i * 4 + 2] = (byte) g;
+            imageData[i * 4 + 3] = (byte) r;
+        }
+        return image;
+    }
+
     public boolean hasNewFrame() {
         return OpenpnpCaptureLibrary.INSTANCE.Cap_hasNewFrame(context, streamId) == 1;
     }
