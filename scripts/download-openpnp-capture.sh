d6fd684b53c33e14402a7682f8368c194a4ff486
Roman Valls Guimera <brainstorm@nopcode.org>
2020-05-02 00:11:24 +1000

    It should just be adding a few lines to copy the binaries to the right place, thanks @vonnieda

--- scripts/download-openpnp-capture.sh
+++ scripts/download-openpnp-capture.sh
@@ -7,11 +7,6 @@ if [ "$#" -ne 1 ]; then
     exit
 fi
 
-# darwin
-# win32-x86
-# win32-x86-64
-# linux-x86
-# linux-x86-64
 rm -rf openpnp-capture
 mkdir -p openpnp-capture
 curl -L -o openpnp-capture/source.tar.gz https://github.com/openpnp/openpnp-capture/archive/$1.tar.gz
@@ -21,8 +16,10 @@ mkdir -p openpnp-capture/binaries/darwin
 mkdir -p openpnp-capture/binaries/win32-x86
 mkdir -p openpnp-capture/binaries/win32-x86-64
 mkdir -p openpnp-capture/binaries/linux-x86-64
+mkdir -p openpnp-capture/binaries/linux-arm64
 
 curl -L -o openpnp-capture/binaries/darwin/libopenpnp-capture.dylib https://github.com/openpnp/openpnp-capture/releases/download/$1/libopenpnp-capture.dylib
 curl -L -o openpnp-capture/binaries/win32-x86/openpnp-capture.dll https://github.com/openpnp/openpnp-capture/releases/download/$1/openpnp-capture_Win32.dll
 curl -L -o openpnp-capture/binaries/win32-x86-64/openpnp-capture.dll https://github.com/openpnp/openpnp-capture/releases/download/$1/openpnp-capture_x64.dll
 curl -L -o openpnp-capture/binaries/linux-x86-64/libopenpnp-capture.so https://github.com/openpnp/openpnp-capture/releases/download/$1/libopenpnp-capture.so
+curl -L -o openpnp-capture/binaries/linux-arm64/libopenpnp-capture-arm64.so https://github.com/openpnp/openpnp-capture/releases/download/$1/libopenpnp-capture-arm64.so
